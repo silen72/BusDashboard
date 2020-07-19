cd4885e8c5702af686a6d4ab63b2cd3a62acfd1f
silen72 <silen72@wolke7.net>
2020-07-19 23:18:11 +0200

    removed vector, limits - gained 260 Bytes RAM

--- include/KOMSI/KOMSICommandListener.h
+++ include/KOMSI/KOMSICommandListener.h
@@ -1,14 +1,29 @@
-/**
- *
- *
- */ 
 
 #pragma once
-#include <stdint.h>
+#include <Arduino.h>
 
 namespace BusDashboard {
     class KomsiCommandListener {
     public:
         virtual void receiveCommand(const uint8_t command, const uint8_t value) = 0;
+    };
+
+    class KomsiCommandListenerNode {
+    public:
+        KomsiCommandListenerNode* next() { return _next; }
+        KomsiCommandListenerNode* append(KomsiCommandListener& item) {
+            _next = new KomsiCommandListenerNode(item);
+            return _next;
+        }
+        KomsiCommandListener* item() { return _item; }
+        KomsiCommandListenerNode(KomsiCommandListener& item):_item(&item), _next(nullptr) {};
+    
+    private:
+        KomsiCommandListener* _item;
+        KomsiCommandListenerNode* _next;
+
+        KomsiCommandListenerNode() = delete;
+        KomsiCommandListenerNode(const KomsiCommandListenerNode&) = delete;
+        KomsiCommandListenerNode& operator=(const KomsiCommandListenerNode&) = delete;
     };
 }
