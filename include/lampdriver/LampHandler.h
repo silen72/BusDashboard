185bd7ef23f7d46c2e324277d2d573ef930942de
silen72 <silen72@wolke7.net>
2020-07-19 21:52:43 +0200

    made Lamphandler more lightweight

--- include/lampdriver/LampHandler.h
+++ include/lampdriver/LampHandler.h
@@ -1,65 +1,14 @@
 #pragma once
 #include <Arduino.h>
-#include <stdint.h>
-#include <vector>
+//#include <stdint.h>
 
 namespace BusDashboard {
-
-    class LampHandler;
-
-    class Lamp {
-        private:
-            LampHandler& _parent;
-            const uint8_t _position; // connected to which logical pin number (0 .. numberOfChips * 8 - 1)
-
-            // disallow creation
-            Lamp() = delete;
-            Lamp(const Lamp&) = delete;
-            Lamp& operator=(const Lamp&) = delete;
-
-        protected:
-            Lamp(LampHandler& parent, const uint8_t position):_parent(parent),_position(position) {}
-
-        public:
-            /**
-             * switches the lamp on
-             */
-            void on() { setState(true); };
-
-            /**
-             * switches the lamp off
-             */
-            void off() { setState(false); };
-
-            /**
-             * queries the logical lamp state
-             * @returns the current logical lamp state (the state the hardware will be switched to on the next update)
-             */
-            bool state() const;
-
-            /**
-             * queries the hardware lamp state
-             * @returns the current hardware lamp state
-             */
-            bool hwstate() const;
-
-            /**
-             * sets the logical lamp state
-             * @param[in] newState
-             */
-            void setState(const bool state);
-
-            /**
-             * @returns the position of this Lamp on the lamp driver
-             */
-            uint8_t position() const { return _position; }
-
-        friend class LampHandler;
-    };
 
 	class LampHandler {
 	
 	public:
+
+        static const uint8_t NUMBER_OF_ICS = 6;
 		/**
 		 * updates the physical state of the lamps to the logical state, if necessary
 		 * communicates with the shift register ICs
@@ -81,41 +30,29 @@ namespace BusDashboard {
 		 */
         const uint8_t pinLatch() const { return _pinLatch; }
 
- 		/**
-		 * @returns the number of 74HC595 chips the lamp driver consists of 
-		 */
-        const uint8_t icCount() const { return _numberOfIcs; }
-
- 		/**
-          * queries the Lamp instanve at a specific pin pair positions
-          * @param[in] postition the position to query
-          * @returns the Lamp instance at that position (may be nullptr)
-          */
-        Lamp * getLampAtPosition(const uint8_t position) { return isValidPosition(position) ? _lamps[position] : nullptr; }
 
  		/**
           * queries whether a given position is valid on the lamp driver
           * @param[in] postition the position to query
           * @returns true, if the given position is valid (note: this does not imply that a Lamp instance exists at that position)
           */
-        bool isValidPosition(const uint8_t position) const { return (position < 8 * _numberOfIcs); }
+        bool isValidPosition(const uint8_t position) const { return (position < 8 * NUMBER_OF_ICS); }
 
-        LampHandler(const uint8_t pinSerial, const uint8_t pinSerialClock, const uint8_t pinLatch, const uint8_t numberOfICs);
+        LampHandler(const uint8_t pinSerial, const uint8_t pinSerialClock, const uint8_t pinLatch);
 
+        void setState(const uint8_t position, const bool state);
+
+        bool state(const uint8_t position, const bool queryHw = false) const;
 
 	protected:
-        void setState(const uint8_t position, const bool state);
-        bool state(const uint8_t position, const bool queryHw = false) const;
 
 
 	private:
         const uint8_t _pinSerial;
         const uint8_t _pinSerialClock;
         const uint8_t _pinLatch;
-        const uint8_t _numberOfIcs;
-        std::vector<Lamp*> _lamps;          // Lamp instances
-        std::vector<uint8_t> _lampstates;   // logical lamp state:  vector of bitarrays, one 8bit-Array per IC
-        std::vector<uint8_t> _lampstatesHw; // hardware lamp state: vector of bitarrays, one 8bit-Array per IC
+        uint8_t _lampstates[NUMBER_OF_ICS];   // logical lamp state:  vector of bitarrays, one 8bit-Array per IC
+        uint8_t _lampstatesHw[NUMBER_OF_ICS]; // hardware lamp state: vector of bitarrays, one 8bit-Array per IC
 
         // disallow creation
 		LampHandler() = delete;
@@ -126,7 +63,5 @@ namespace BusDashboard {
           * updates the hw state of the lamp pins with the logical lamp states
           */
         void writeLampState();
-
-        friend class Lamp;
 	};
 }
