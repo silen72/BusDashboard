03996d534e219fb28b85b6958527930efb26560f
silen72 <silen72@wolke7.net>
2020-07-19 22:57:34 +0200

    moved two line code to header

--- src/buttonmatrix/ButtonListenerNode.cpp
+++
@@ -1,8 +0,0 @@
-#include "buttonmatrix/ButtonListener.h"
-
-namespace BusDashboard {
-    ButtonListenerNode* ButtonListenerNode::append(ButtonListener& buttonlistener) {
-        _next = new ButtonListenerNode(buttonlistener);
-        return _next;
-    }
-}
