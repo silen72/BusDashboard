1b7bbdf7a4df0ef7f4f56662375e03520f57aebe
silen72 <silen72@wolke7.net>
2020-07-19 13:57:11 +0200

    fixed typo in comment

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ enum ArduinoPins {
 };
 
 void setup() {
-  SPI.begin();            // initialize communication with the SPIU devices (button matrix and CAN-bus driver)
+  SPI.begin();            // initialize communication with the SPI devices (button matrix and CAN-bus driver)
   Serial.begin(115200);   // initialize communication with the PC (only in:KOMSI)
   Keyboard.begin();       // initialize communication with the PC (only out: Keyboard)
 }
