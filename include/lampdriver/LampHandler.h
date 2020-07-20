f3dfc4fa11d3973a4ed846c4db7a3a7fe3f66d0b
silen72 <silen72@wolke7.net>
2020-07-20 17:47:59 +0200

    minor grooming

--- include/lampdriver/LampHandler.h
+++ include/lampdriver/LampHandler.h
@@ -1,41 +1,41 @@
 #pragma once
 #include <Arduino.h>
-//#include <stdint.h>
 
 namespace BusDashboard {
 
-	class LampHandler {
-	
-	public:
+class LampHandler {
+
+  public:
 
         static const uint8_t NUMBER_OF_ICS = 6;
-		/**
-		 * updates the physical state of the lamps to the logical state, if necessary
-		 * communicates with the shift register ICs
-		 */
-		void update();
 
- 		/**
-		 * @returns the pin number to which SI is connected 
-		 */
+        /**
+         * updates the physical state of the lamps to the logical state, if necessary
+         * communicates with the shift register ICs
+         */
+        void update();
+
+        /**
+         * @returns the pin number to which SI is connected 
+         */
         const uint8_t pinSerial() const { return _pinSerial; }
 
- 		/**
-		 * @returns the pin number to which SCK is connected 
-		 */
+        /**
+         * @returns the pin number to which SCK is connected 
+         */
         const uint8_t pinSerialClock() const { return _pinSerialClock; }
 
- 		/**
-		 * @returns the pin number to which RCK is connected 
-		 */
+        /**
+         * @returns the pin number to which RCK is connected 
+         */
         const uint8_t pinLatch() const { return _pinLatch; }
 
 
- 		/**
-          * queries whether a given position is valid on the lamp driver
-          * @param[in] postition the position to query
-          * @returns true, if the given position is valid (note: this does not imply that a Lamp instance exists at that position)
-          */
+        /**
+         * queries whether a given position is valid on the lamp driver
+         * @param[in] postition the position to query
+         * @returns true, if the given position is valid (note: this does not imply that a Lamp instance exists at that position)
+         */
         bool isValidPosition(const uint8_t position) const { return (position < 8 * NUMBER_OF_ICS); }
 
         LampHandler(const uint8_t pinSerial, const uint8_t pinSerialClock, const uint8_t pinLatch);
@@ -44,10 +44,10 @@ namespace BusDashboard {
 
         bool state(const uint8_t position, const bool queryHw = false) const;
 
-	protected:
+  protected:
 
 
-	private:
+  private:
         const uint8_t _pinSerial;
         const uint8_t _pinSerialClock;
         const uint8_t _pinLatch;
@@ -55,13 +55,13 @@ namespace BusDashboard {
         uint8_t _lampstatesHw[NUMBER_OF_ICS]; // hardware lamp state: vector of bitarrays, one 8bit-Array per IC
 
         // disallow creation
-		LampHandler() = delete;
+        LampHandler() = delete;
         LampHandler(const LampHandler&) = delete;
         LampHandler& operator=(const LampHandler&) = delete;
 
- 		/**
-          * updates the hw state of the lamp pins with the logical lamp states
-          */
+        /**
+         * updates the hw state of the lamp pins with the logical lamp states
+         */
         void writeLampState();
-	};
+        };
 }
