bc49dc86d8ad35bd36cc4e69cca4b65a85e46aaa
silen72 <silen72@wolke7.net>
2020-10-18 20:35:09 +0200

    removed obsolete code and comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,9 @@
 #include <SPI.h>
 #include "dashboard/Dashboard.h"
 
-#include "debug/LampTester.h"
-#include "buttonmatrix/ButtonHandler.h"
-#include "lampdriver/LampHandler.h"
-#include "keyboard/KeyboardHandler.h"
-#include "KOMSI/KOMSIHandler.h"
-#include "CANBus/CANBus.h"
-
 using namespace BusDashboard;
 
 Dashboard &dashboard = Dashboard::instance();
-
-//LampTester debuglamptester(dashboard.lampHandler());
 
 void setup()
 {
@@ -29,22 +20,7 @@ void setup()
 #warning to configure Serial debugging use the build_flags = -D SerialDebug line in platformio.ini
 #endif
 
-//uint8_t lampPosition = 0;
-//uint8_t prevpos = lampPosition + 1;
-//unsigned long m = millis();
-
 void loop()
 {
-  /*
-  // moving lights for lamp 1 to 4 (0 ..3); next every second
-  if ((millis() - m) > 1000UL) {
-    dashboard.lampHandler().setState(lampPosition, true);
-    dashboard.lampHandler().setState(prevpos, false);
-    prevpos = lampPosition;
-    lampPosition++;
-    if (lampPosition >= 4) lampPosition = 0;
-  }
-  */
-
   dashboard.update();
 }
